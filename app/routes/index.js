472f9d33b1439fad18e39c72ac608c48878bbfbf
bairdel <beck.sewart@warwick.ac.uk>
2023-02-27 11:18:07 +0000

    can now run website for testing

--- app/routes/index.js
+++ app/routes/index.js
@@ -10,7 +10,7 @@ const router = express.Router();
  */
 router.get("/", (req, res) => {
   console.dir(req.query);
-  let language = req.query?.language;
+  let language = req.query.language;
   if (!language) {
     language = "english";
   }
